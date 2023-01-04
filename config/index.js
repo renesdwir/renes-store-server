0bb3db4025f7cc3ebbabfbad43fc6fa7b9baad69
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-01-04 21:42:25 +0700

    setting db connection

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@ const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
   serviceName: process.env.SERVICE_NAME,
-  urlDB: process.env.MONGO_URL,
+  urlDb: process.env.MONGO_URL,
 };
