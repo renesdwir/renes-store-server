7e419e397e0bc6100d9cdd9b87b78260e55849aa
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-01-09 21:16:16 +0700

    create post category contoller

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,3 +1,5 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
@@ -10,6 +12,16 @@ module.exports = {
   viewCreate: async (req, res) => {
     try {
       res.render("admin/category/create");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+      let category = await Category({ name });
+      await category.save();
+      res.redirect("/category");
     } catch (error) {
       console.log(error);
     }
