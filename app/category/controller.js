c986b396be7a025fdfaf2706c0df120a7242c8a1
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-01-10 21:31:23 +0700

    add fitur edit category

--- app/category/controller.js
+++ app/category/controller.js
@@ -26,4 +26,23 @@ module.exports = {
       console.log(error);
     }
   },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOne({ _id: id });
+      res.render("admin/category/edit", { category });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+      const category = await Category.findOneAndUpdate({ _id: id }, { name });
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
