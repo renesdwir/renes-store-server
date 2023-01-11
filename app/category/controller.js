c8069f424ce2aa723438dfd1eef9877a348f3524
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-01-11 22:28:15 +0700

    add fitur delete category

--- app/category/controller.js
+++ app/category/controller.js
@@ -37,9 +37,17 @@ module.exports = {
   },
   actionEdit: async (req, res) => {
     try {
-      const { id } = req.params;
       const { name } = req.body;
       const category = await Category.findOneAndUpdate({ _id: id }, { name });
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOneAndRemove({ _id: id });
       res.redirect("/category");
     } catch (error) {
       console.log(error);
--- app/category/router.js
+++ app/category/router.js
@@ -6,6 +6,7 @@ const {
   actionCreate,
   viewEdit,
   actionEdit,
+  actionDelete,
 } = require("./controller");
 
 /* GET home page. */
@@ -14,5 +15,6 @@ router.get("/create", viewCreate);
 router.post("/create", actionCreate);
 router.get("/edit/:id", viewEdit);
 router.put("/edit/:id", actionEdit);
+router.delete("/delete/:id", actionDelete);
 
 module.exports = router;
