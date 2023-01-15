2b24d24fc646ced8ffee8d46fc3d5906a482b934
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-01-15 21:01:22 +0700

    voucher model

---
+++ app/voucher/model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+let voucherSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Game name is required"],
+  },
+  status: {
+    type: String,
+    enum: ["Y", "N"],
+    default: "Y",
+  },
+  thumbnail: {
+    type: String,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  nominals: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Nominal",
+    },
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+module.exports = mongoose.model("Voucher", voucherSchema);
