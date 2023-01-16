d3c74f190ed784541f6a65a230648a6b0692117b
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-01-16 21:42:38 +0700

    routing nominal viewcreate

--- app/voucher/router.js
+++ app/voucher/router.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const {
   index,
-  //   viewCreate,
+  viewCreate,
   //   actionCreate,
   //   viewEdit,
   //   actionEdit,
@@ -11,7 +11,7 @@ const {
 
 /* GET home page. */
 router.get("/", index);
-// router.get("/create", viewCreate);
+router.get("/create", viewCreate);
 // router.post("/create", actionCreate);
 // router.get("/edit/:id", viewEdit);
 // router.put("/edit/:id", actionEdit);
