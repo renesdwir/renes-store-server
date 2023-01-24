df59c86802cf1c2139569f601a1694f3dcd80ef7
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-01-24 21:16:03 +0700

    model payment init

---
+++ app/payment/model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+let paymentSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    require: [true, "Payment type is required"],
+  },
+  status: {
+    type: String,
+    enum: ["Y", "N"],
+    default: "Y",
+  },
+  banks: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Bank",
+  },
+});
+module.exports = mongoose.model("Payment", paymentSchema);
