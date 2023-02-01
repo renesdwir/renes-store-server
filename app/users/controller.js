cb4a1507277465a4cf98cf6f5ea09b57add4f79d
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-02-01 20:45:20 +0700

    applied to controller and router

--- app/users/controller.js
+++ app/users/controller.js
@@ -3,12 +3,14 @@ const bcrypt = require("bcryptjs");
 module.exports = {
   viewSignin: async (req, res) => {
     try {
-      console.log("masukkkkkk");
       const alertMessage = req.flash("alertMessage");
       const alertStatus = req.flash("alertStatus");
       const alert = { message: alertMessage, status: alertStatus };
-
-      res.render("./admin/users/view_signin", { alert });
+      if (req.session.user === null || req.session.user === undefined) {
+        res.render("./admin/users/view_signin", { alert });
+      } else {
+        res.redirect("/dashboard");
+      }
     } catch (err) {
       req.flash("alertMessage", `${err.message}`);
       req.flash("alertStatus", "danger");
@@ -19,24 +21,36 @@ module.exports = {
   actionSignIn: async (req, res) => {
     try {
       const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      if (!user) {
-        req.flash("alertMessage", `Incorrect Email or Password`);
-        req.flash("alertStatus", "danger");
-        res.redirect("/");
-      }
-      if (user.status === "N") {
-        req.flash("alertMessage", `User Inactive`);
-        req.flash("alertStatus", "danger");
-        res.redirect("/");
-      }
-      const checkPassword = await bcrypt.compare(password, user.password);
-      if (!checkPassword) {
-        req.flash("alertMessage", `Incorrect Email or Password`);
+      if (!email || !password) {
+        req.flash("alertMessage", `Email and password undefined`);
         req.flash("alertStatus", "danger");
         res.redirect("/");
       } else {
-        res.redirect("/dashboard");
+        const user = await User.findOne({ email });
+        if (!user) {
+          req.flash("alertMessage", `Incorrect Email or Password`);
+          req.flash("alertStatus", "danger");
+          res.redirect("/");
+        }
+        if (user.status === "N") {
+          req.flash("alertMessage", `User Inactive`);
+          req.flash("alertStatus", "danger");
+          res.redirect("/");
+        }
+        const checkPassword = await bcrypt.compare(password, user.password);
+        if (!checkPassword) {
+          req.flash("alertMessage", `Incorrect Email or Password`);
+          req.flash("alertStatus", "danger");
+          res.redirect("/");
+        } else {
+          req.session.user = {
+            id: user._id,
+            email: user.email,
+            status: user.status,
+            name: user.name,
+          };
+          res.redirect("/dashboard");
+        }
       }
     } catch (err) {
       req.flash("alertMessage", `${err.message}`);
