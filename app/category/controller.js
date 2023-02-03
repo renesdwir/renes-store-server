f892e8686c3ef959ec6dd41476807d098a64ef79
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-02-03 22:25:00 +0700

    category & nominal add title and user login

--- app/category/controller.js
+++ app/category/controller.js
@@ -8,7 +8,12 @@ module.exports = {
 
       const alert = { message: alertMessage, status: alertStatus };
       const category = await Category.find();
-      res.render("./admin/category/view_category", { category, alert });
+      res.render("./admin/category/view_category", {
+        category,
+        alert,
+        name: req.session.user.name,
+        title: "Category Page",
+      });
     } catch (err) {
       req.flash("alertMessage", `${err.message}`);
       req.flash("alertStatus", "danger");
@@ -18,7 +23,10 @@ module.exports = {
   },
   viewCreate: async (req, res) => {
     try {
-      res.render("admin/category/create");
+      res.render("admin/category/create", {
+        name: req.session.user.name,
+        title: "Create Category",
+      });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
       req.flash("alertStatus", "danger");
@@ -46,7 +54,11 @@ module.exports = {
     try {
       const { id } = req.params;
       const category = await Category.findOne({ _id: id });
-      res.render("admin/category/edit", { category });
+      res.render("admin/category/edit", {
+        category,
+        name: req.session.user.name,
+        title: "Edit Category",
+      });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
       req.flash("alertStatus", "danger");
--- app/nominal/controller.js
+++ app/nominal/controller.js
@@ -8,7 +8,12 @@ module.exports = {
 
       const alert = { message: alertMessage, status: alertStatus };
       const nominal = await Nominal.find();
-      res.render("./admin/nominal/view_nominal", { nominal, alert });
+      res.render("./admin/nominal/view_nominal", {
+        nominal,
+        alert,
+        name: req.session.user.name,
+        title: "Nominal Page",
+      });
     } catch (err) {
       req.flash("alertMessage", `${err.message}`);
       req.flash("alertStatus", "danger");
@@ -18,7 +23,10 @@ module.exports = {
   },
   viewCreate: async (req, res) => {
     try {
-      res.render("admin/nominal/create");
+      res.render("admin/nominal/create", {
+        name: req.session.user.name,
+        title: "Create Nominal",
+      });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
       req.flash("alertStatus", "danger");
@@ -51,7 +59,11 @@ module.exports = {
       const { id } = req.params;
       const nominal = await Nominal.findOne({ _id: id });
       console.log(nominal);
-      res.render("admin/nominal/edit", { nominal });
+      res.render("admin/nominal/edit", {
+        nominal,
+        name: req.session.user.name,
+        title: "Edit Nominal",
+      });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
       req.flash("alertStatus", "danger");
