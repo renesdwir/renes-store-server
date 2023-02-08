dfac49ca6ff848721021b5b143643c71f39b024f
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-02-08 21:56:15 +0700

    add route actionstatus

--- app/transaction/router.js
+++ app/transaction/router.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const { index } = require("./controller");
+const { index, actionStatus } = require("./controller");
 
 /* GET home page. */
 router.get("/", index);
+router.put("/status/:id", actionStatus);
 
 module.exports = router;
