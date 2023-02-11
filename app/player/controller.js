4f968b3b078eb04763d0559aa1f4215e4fbc6801
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-02-11 22:20:06 +0700

    detail page player

--- app/player/controller.js
+++ app/player/controller.js
@@ -14,4 +14,21 @@ module.exports = {
         .json({ message: error.message || "Internal Server Error" });
     }
   },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const voucher = await Voucher.find({ _id: id })
+        .populate("category")
+        .populate("nominal")
+        .populate("user", "_id name phoneNumber");
+      if (!voucher) {
+        return res.status(404).json({ message: "voucher game not found.!" });
+      }
+      res.status(200).json({ data: voucher });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal Server Error" });
+    }
+  },
 };
