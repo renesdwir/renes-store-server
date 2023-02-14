7bd8ef2408d6eb6472575c0d02b2cfbd2884599a
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-02-14 21:36:20 +0700

    save file

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,10 +1,43 @@
 const Player = require("../player/model");
+const path = require("path");
+const fs = require("fs");
+const config = require("../../config");
 module.exports = {
   signUp: async (req, res, next) => {
     try {
       const payload = req.body;
       if (req.file) {
-        console.log("1");
+        let tmp_path = req.file.path;
+        let originExt =
+          req.file.originalname.split(".")[
+            req.file.originalname.split(".").length - 1
+          ];
+        let fileName = req.file.filename + "." + originExt;
+        let target_path = path.resolve(
+          config.rootPath,
+          `public/uploads/${fileName}`
+        );
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
+
+        src.pipe(dest);
+        src.on("end", async () => {
+          try {
+            const voucher = new Voucher({
+              name,
+              category,
+              nominals,
+              thumbnail: fileName,
+            });
+            await voucher.save();
+
+            req.flash("alertMessage", "Data Created Successfully");
+            req.flash("alertStatus", "success");
+            res.redirect("/voucher");
+          } catch (error) {
+            console.log(error);
+          }
+        });
       } else {
         let player = new Player(payload);
         await player.save();
