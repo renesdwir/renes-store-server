b1003f1f7bc793f440254ae1b5fd06ec0e692c9b
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-02-28 21:31:47 +0700

    checkout API

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,6 +1,9 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
 const Category = require("../category/model");
+const Nominal = require("../nominal/model");
+const Payment = require("../payment/model");
+const Bank = require("../bank/model");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -36,6 +39,31 @@ module.exports = {
     try {
       const category = await Category.find();
       res.status(200).json({ data: category });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal Server Error" });
+    }
+  },
+  checkout: async (req, res) => {
+    try {
+      const { accountUser, name, nominal, voucher, payment, bank } = req.body;
+      const res_voucher = await Voucher.findOne({ _id: voucher })
+        .select("name category _id thumbnail user")
+        .populate("category")
+        .populate("user");
+
+      if (!res_voucher)
+        return res.status(404).json({ message: "voucher game not found" });
+
+      const res_nominal = await Nominal.findOne({ _id: nominal });
+      if (!res_nominal)
+        return res.status(404).json({ message: "nominal not found" });
+      const res_payment = await Payment.findOne({ _id: payment });
+      if (!res_payment)
+        return res.status(404).json({ message: "payment not found" });
+      const res_bank = await Bank.findOne({ _id: bank });
+      if (!res_bank) return res.status(404).json({ message: "bank not found" });
     } catch (error) {
       res
         .status(500)
