fa9d68d9f684896ee952d7c7f70a056d11142995
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-01 22:03:36 +0700

    add checkout router

--- app/player/router.js
+++ app/player/router.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var router = express.Router();
-const { landingPage, detailPage, category } = require("./controller");
+const { landingPage, detailPage, category, checkout } = require("./controller");
 
 /* GET home page. */
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
+router.post("/checkout", checkout);
 
 module.exports = router;
