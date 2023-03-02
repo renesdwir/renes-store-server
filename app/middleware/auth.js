1b2947eb0d0416f923ae252663e40f8f729bb80b
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-02 21:56:02 +0700

    add middleware isLoginPlayer

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,3 +1,7 @@
+const config = require("../../config");
+const jwt = require("jsonwebtoken");
+const Player = require("../player/model");
+
 module.exports = {
   isLoginAdmin: (req, res, next) => {
     if (req.session.user === null || req.session.user === undefined) {
@@ -9,9 +13,17 @@ module.exports = {
       res.redirect("/");
     } else next();
   },
-  isLoginPlayer: (req, res, next) => {
+  isLoginPlayer: async (req, res, next) => {
     try {
-      const token = req.header.authorization;
+      const token = req.get("Authorization") ? req.get("Authorization") : null;
+      const data = jwt.verify(token, config.jwtKey);
+      const player = await Player.findOne({ _id: data.player.id });
+      if (!player) {
+        throw new Error();
+      }
+      req.player = player;
+      req.token = token;
+      next();
     } catch (error) {
       res.status(401).json({
         error: "Not authorize to access this resource",
