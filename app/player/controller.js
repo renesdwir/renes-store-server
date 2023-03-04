ce5a7969f844d074cb04679c5d3871dd2a4e3f87
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-04 22:34:05 +0700

    add aggregate

--- app/player/controller.js
+++ app/player/controller.js
@@ -124,8 +124,18 @@ module.exports = {
         };
       }
       const history = await Transaction.find(criteria);
+      let total = await Transaction.aggregate([
+        { $match: criteria },
+        {
+          $group: {
+            _id: null,
+            value: { $sum: "$value" },
+          },
+        },
+      ]);
       res.status(200).json({
         data: history,
+        total: total.length ? total[0].value : 0,
       });
     } catch (error) {
       res
