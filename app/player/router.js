48a09f428cbdb191092ce20d8e41b305742c944f
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-03-04 22:27:58 +0700

    add router get history

--- app/player/router.js
+++ app/player/router.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var router = express.Router();
-const { landingPage, detailPage, category, checkout } = require("./controller");
+const {
+  landingPage,
+  detailPage,
+  category,
+  checkout,
+  history,
+} = require("./controller");
 const { isLoginPlayer } = require("../middleware/auth");
 
 /* GET home page. */
@@ -8,5 +14,6 @@ router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
 
 module.exports = router;
